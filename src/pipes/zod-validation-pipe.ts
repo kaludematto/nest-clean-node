5d4897518956751c51c241a529045150a7413617
kaludematto <kalu.dematto@gmail.com>
2025-02-25 17:16:00 -0300

    refactor zod validation

--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -1,5 +1,6 @@
 import { PipeTransform, BadRequestException } from '@nestjs/common'
 import { ZodError, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
