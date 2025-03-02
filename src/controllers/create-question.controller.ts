2ec4d2452447d4f88abd95f785ac59f49e237970
kaludematto <kalu.dematto@gmail.com>
2025-03-02 20:38:05 -0300

    update questions controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Controller('/questions')
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
 
