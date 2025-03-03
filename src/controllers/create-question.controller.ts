81ca184db489f84230aaba16a0ba50bd8fb2dac6
kaludematto <kalu.dematto@gmail.com>
2025-03-03 20:31:55 -0300

    nerw update

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common'
+import { Controller, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { UserPayload } from 'src/auth/jwt.strategy'
