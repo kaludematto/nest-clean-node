3f42355be5dad3b50f8f980df9593d6292b31203
kaludematto <kalu.dematto@gmail.com>
2025-03-09 20:35:52 -0300

    lint

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -5,7 +5,6 @@ import { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
   const configService = app.get<ConfigService<Env, true>>(ConfigService)
   const port = configService.get('PORT', { infer: true })
 
