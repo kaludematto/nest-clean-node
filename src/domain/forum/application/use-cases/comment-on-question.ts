ed9309c8deb575c093bb387780af38afd17a3dd8
kaludematto <kalu.dematto@gmail.com>
2025-03-09 19:55:47 -0300

    add new folders

---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,48 @@
+import { QuestionsRepository } from '../repositories/question-repository'
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repositories/question-comment-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Either, left, right } from '@/core/either'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  content: string
+}
+
+type CommentOnQuestionUseCaseResponse = Either<
+  ResourceNotFoundError,
+  {
+    questionComment: QuestionComment
+  }
+>
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionComments: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    questionId,
+    content,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId)
+
+    if (!question) {
+      return left(new ResourceNotFoundError())
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityID(authorId),
+      questionId: new UniqueEntityID(questionId),
+      content,
+    })
+
+    await this.questionComments.create(questionComment)
+
+    return right({ questionComment })
+  }
+}
---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,40 @@
+import { expect, it } from 'vitest'
+import { CreateQuestionUseCase } from './create-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+let sut: CreateQuestionUseCase
+
+describe('Create Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository,
+    )
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to create a question', async () => {
+    const result = await sut.execute({
+      authorId: '1',
+      title: 'New Question',
+      content: 'Nova Resposta',
+      attachmentsIds: ['1', '2'],
+    })
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question)
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems,
+    ).toHaveLength(2)
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems,
+    ).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+    ])
+  })
+})
